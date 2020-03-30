efa43aac434332069e84bb899ef43600ef519f76
Mr.R00ki3 <43641228+LuqmaanSiddeeq@users.noreply.github.com>
2020-03-30 19:00:17 +0530

    Add files via upload

---
+++ precache-manifest.3e2a26a01ffd75cbe21f8916eb67398a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d3181b8be6ae3c5cd84321b7ca8ba8",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "42eed2067437d433dab1",
+    "url": "/covid19/static/css/2.83767612.chunk.css"
+  },
+  {
+    "revision": "acbd79a5058e84758559",
+    "url": "/covid19/static/css/main.585bd29b.chunk.css"
+  },
+  {
+    "revision": "42eed2067437d433dab1",
+    "url": "/covid19/static/js/2.b6dd15e0.chunk.js"
+  },
+  {
+    "revision": "90fb4deb912422b947a6490378160cce",
+    "url": "/covid19/static/js/2.b6dd15e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbd79a5058e84758559",
+    "url": "/covid19/static/js/main.803b002c.chunk.js"
+  },
+  {
+    "revision": "eda5e4916d23ad5b37f0",
+    "url": "/covid19/static/js/runtime-main.34187ac3.js"
+  },
+  {
+    "revision": "b3fc0ba54ccc330ba3913eea53262059",
+    "url": "/covid19/static/media/1-1.b3fc0ba5.png"
+  },
+  {
+    "revision": "0451ca0ab5e69adcf1e347b58653f786",
+    "url": "/covid19/static/media/1-3.0451ca0a.png"
+  },
+  {
+    "revision": "9f82160662f915ac875d7bd5fab2ac81",
+    "url": "/covid19/static/media/2-3.9f821606.png"
+  },
+  {
+    "revision": "17d9954627fadc9d520614bb84428cdc",
+    "url": "/covid19/static/media/image1.17d99546.png"
+  }
+]);
